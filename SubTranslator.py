c8d4d175926e3b8b72f568cb61a004f6e66a40f9
hooyuser <hooyuser@outlook.com>
2020-05-17 02:14:21 +0800

    srt2srt

--- SubTranslator.py
+++ SubTranslator.py
@@ -3,6 +3,7 @@ import hashlib
 import urllib
 import random
 import json
+import re
 
 
 def translate(q, from_lang='auto', to_lang='zh'):
@@ -14,7 +15,7 @@ def translate(q, from_lang='auto', to_lang='zh'):
     salt = random.randint(32768, 65536)
     sign = app_id + q + str(salt) + secret_key
     sign = hashlib.md5(sign.encode()).hexdigest()
-    myurl = myurl + '?app_id=' + app_id + '&q=' + urllib.parse.quote(
+    myurl = myurl + '?appid=' + app_id + '&q=' + urllib.parse.quote(
         q) + '&from=' + from_lang + '&to=' + to_lang + '&salt=' + str(
         salt) + '&sign=' + sign
 
@@ -27,12 +28,29 @@ def translate(q, from_lang='auto', to_lang='zh'):
         result_all = response.read().decode("utf-8")
         result = json.loads(result_all)
         dst = result["trans_result"][0]["dst"]
-        print(dst)
+        return dst
 
     except Exception as e:
         print(e)
+
     finally:
         if httpClient:
             httpClient.close()
 
 
+def srt2srt(input_file, from_lang='auto', to_lang='zh'):
+    with open(input_file, 'r', encoding='utf-8') as f_in:
+        output_file = input_file[:-4] + '_trans' + input_file[-4:]
+        with open(output_file, 'w', encoding='utf-8') as f_out:
+            for line in f_in:
+                pattern = re.compile('^\d*$|\d{2}:\d{2}:\d{2},\d{3}|\n')
+                if re.match(pattern, line):
+                    f_out.write(line)
+                else:
+                    line_trans = translate(line.strip(), from_lang, to_lang)
+                    print(line_trans)
+                    f_out.write(line_trans + '\n')
+
+
+input_srt = '25 - Applying Colors and Patterns.srt'
+srt2srt(input_srt)
