15a743b3ec6e311221aef16132d802093cffa7ef
hooyuser <hooyuser@outlook.com>
2020-05-17 23:28:53 +0800

    srt2ass

--- SubTranslator.py
+++ SubTranslator.py
@@ -4,6 +4,7 @@ import urllib
 import random
 import json
 import re
+import toml
 
 
 def translate(q, from_lang='auto', to_lang='zh'):
@@ -38,10 +39,15 @@ def translate(q, from_lang='auto', to_lang='zh'):
             httpClient.close()
 
 
-def srt2srt_by_one(input_file, from_lang='auto', to_lang='zh', bi_sub=True):
+def delete_end_punc(str):  # Delete punctuations at the end of sentences
+    punctuation = '，。？！；：,.?!:;'
+    return re.sub(re.compile(r'[{}]+$'.format(punctuation)), "", str)
+
+
+def srt_trans_srt_by_one(input_file, from_lang='auto', to_lang='zh', bi_sub=True):
     with open(input_file, 'r', encoding='utf-8') as f_in:
         output_file = input_file[:-4] + '_trans' + input_file[-4:]
-        with open(output_file, 'w', encoding='utf-8') as f_out:
+        with open('Output/' + output_file, 'w', encoding='utf-8') as f_out:
             for line in f_in:
                 pattern = re.compile('^\d*$|\d{2}:\d{2}:\d{2},\d{3}')
                 if re.match(pattern, line):
@@ -54,7 +60,15 @@ def srt2srt_by_one(input_file, from_lang='auto', to_lang='zh', bi_sub=True):
                     f_out.write(line_trans + '\n')
 
 
-def srt2srt(input_file, from_lang='auto', to_lang='zh', bi_sub=True):
+def srt_trans_srt(input_file, from_lang='auto', to_lang='zh', bi_sub=True, from_lang_top=True):
+    """
+    Translate the language in the input file into another language
+    :param input_file: the srt file to be translated
+    :param from_lang: the language of the input file
+    :param to_lang: the language of the output file
+    :param bi_sub: whether use bi-language subtitle
+    :param from_lang_top: whether from_lang subtitles are higher than to_lang subtitles in the output file
+    """
     sub_list, format_list = [], []
     output_file = input_file[:-4] + '_trans' + input_file[-4:]
     with open(input_file, 'r', encoding='utf-8') as f_in:
@@ -75,15 +89,90 @@ def srt2srt(input_file, from_lang='auto', to_lang='zh', bi_sub=True):
     if not len(format_list) == len(sub_list) == len(trans_list):
         print('Length not match!')
     else:
-        with open(output_file, 'w', encoding='utf-8') as f_out:
+        with open('Output/' + output_file, 'w', encoding='utf-8') as f_out:
             for i, line in enumerate(format_list):
-                f_out.write(line)
+                f_out.write(format_list[i])
+                trans_line = delete_end_punc(trans_list[i])
                 if bi_sub:
-                    f_out.write(sub_list[i] + '\n')
-                punctuation = '，。？！；：,.?!:;'  # Delete punctuations at the end of sentences
-                trans_line = re.sub(re.compile(r'[{}]+$'.format(punctuation)), "", trans_list[i])
-                f_out.write(trans_line + '\n\n')
+                    if from_lang_top:
+                        f_out.write(sub_list[i] + '\n')
+                        f_out.write(trans_line + '\n\n')
+                    else:
+                        f_out.write(trans_line + '\n')
+                        f_out.write(sub_list[i] + '\n\n')
+                else:
+                    f_out.write(trans_line + '\n\n')
 
 
-input_srt = '07.srt'
-srt2srt(input_srt)
+def srt_t2ass_t(time):
+    time_list = list(map(int, re.split(re.compile('[^0-9]+'), time)))
+    ms_s = time_list[3] + time_list[2] * 1000 + time_list[1] * 1000 * 60 + time_list[0] * 1000 * 60 * 60
+    ms_e = time_list[7] + time_list[6] * 1000 + time_list[5] * 1000 * 60 + time_list[4] * 1000 * 60 * 60
+    ms_sr = int(round(ms_s / 10) * 10)
+    ms_er = int(round(ms_e / 10) * 10)
+    hour_s = ms_sr // (1000 * 60 * 60)
+    hour_e = ms_er // (1000 * 60 * 60)
+    minute_s = ms_sr % (1000 * 60 * 60) // (1000 * 60)
+    minute_e = ms_er % (1000 * 60 * 60) // (1000 * 60)
+    second_s = ms_sr % (1000 * 60 * 60) % (1000 * 60) // 1000
+    second_e = ms_er % (1000 * 60 * 60) % (1000 * 60) // 1000
+    cs_s = ms_sr % (1000 * 60 * 60) % (1000 * 60) % 1000 // 10
+    cs_e = ms_er % (1000 * 60 * 60) % (1000 * 60) % 1000 // 10
+    ass_s = '{}:{}:{}.{},'.format(str(hour_s), str(minute_s).zfill(2), str(second_s).zfill(2), str(cs_s).zfill(2))
+    ass_e = '{}:{}:{}.{}'.format(str(hour_e), str(minute_e).zfill(2), str(second_e).zfill(2), str(cs_e).zfill(2))
+    return ass_s + ass_e
+
+
+def srt2ass(input_file, language, swap=False):  # from_lang_top:
+    """
+    Convert srt to ass
+    :param input_file:
+    :param language: the language of the input srt file. ex. 'zh_en'
+    :param from_lang_top: whether from_lang subtitles are higher than to_lang subtitles in the ass
+    """
+    config = toml.load('config.toml')
+    time_list = []
+    output_file = re.search(re.compile('\w+\.'), input_file).group() + 'ass'
+
+    if len(language) > 2 and re.search(re.compile(r'zh'), language):
+        high_line, low_line = language[:2] + '_line', language[-2:] + '_line'
+        high_sub_list, low_sub_list = [], []
+        with open(input_file, 'r', encoding='utf-8') as f_in:
+            sub_flag = 0
+            for line in f_in:
+                if re.search(re.compile(r'^\d*\n$'), line):
+                    continue
+                elif re.search(re.compile(r'^\d{2}:\d{2}:\d{2},\d{3}'), line) and not sub_flag:
+                    time_list.append(line.strip())
+                    sub_flag = 1
+                elif sub_flag == 1:
+                    high_sub_list.append(line)
+                    sub_flag = 2
+                elif sub_flag == 2:
+                    low_sub_list.append(line)
+                    sub_flag = 0
+                else:
+                    print('Unidentified line: {}'.format(line))
+        if not len(time_list) == len(high_sub_list) == len(low_sub_list):
+            print('Length not match!')
+        else:
+            with open('Output/' + output_file, 'w', encoding='utf-8') as f_out:
+                if language == 'zh_en' or language == 'en_zh':
+                    f_out.write(config[language + '_ass']['header'] + '\n')
+                    for i, line in enumerate(time_list):
+                        ass_t = srt_t2ass_t(time_list[i])
+                        ass_t_p = re.compile('\d+:\d{2}:\d{2}.\d{2},\d+:\d{2}:\d{2}.\d{2}')
+                        high_sub = re.sub(ass_t_p, ass_t, config[language + '_ass'][high_line]) + high_sub_list[i]
+                        low_sub = re.sub(ass_t_p, ass_t, config[language + '_ass'][low_line]) + low_sub_list[i]
+                        if swap:
+                            f_out.write(low_sub)
+                            f_out.write(high_sub)
+                        else:
+                            f_out.write(high_sub)
+                            f_out.write(low_sub)
+
+
+# input_srt = 'Examples/07.srt'
+# srt_trans_srt(input_srt)
+input_srt = 'Examples/07_trans.srt'
+srt2ass(input_srt, 'en_zh')
